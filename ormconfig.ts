42e66329251dbd17c68b681e3a01f97b042a46ff
jeonghaeman <haemong_2@naver.com>
2022-12-28 20:25:01 +0900

    initialize-setting

---
+++ ormconfig.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+dotenv.config();
+
+export const typeORMConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    type: 'mysql',
+    url: configService.get('DB_URL'),
+    host: configService.get('DB_HOST'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_DATABASE'),
+    entities: ['dist/**/entities/*{.js,.ts}'],
+    synchronize: true,
+  }),
+  dataSourceFactory: async (options) => {
+    const dataSource = await new DataSource(options).initialize();
+    return dataSource;
+  },
+};
