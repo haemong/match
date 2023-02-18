be0f05e2486cb9ad879ca71a6bf62fbb31f633c9
iflov <wjdgusxo99@gmail.com>
2023-02-18 15:29:08 +0900

    branch 변경

--- src/comments/comments.controller.spec.ts
+++ src/comments/comments.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsController } from './comments.controller';
+import { CommentsService } from 'src/comments/comments.service';
 
 describe('CommentsController', () => {
   let controller: CommentsController;
@@ -7,6 +8,19 @@ describe('CommentsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentsController],
+      providers: [
+        CommentsService,
+        {
+          provide: CommentsService,
+          useValue: {
+            createComment: jest.fn(),
+            getComment: jest.fn(),
+            updateComment: jest.fn(),
+            deleteComment: jest.fn(),
+            likeComment: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<CommentsController>(CommentsController);
